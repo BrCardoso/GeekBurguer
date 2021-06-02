9f7d6ff35d0dc7ebe76a6d009bab3127ed18a4c1
Bruna Cardoso <bruna.cardoso@divicom.com.br>
2021-06-01 23:01:50 -0300

    Atividade  aula 1

---
+++ GeekBurguer.Products.Contract/Models/Item.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace GeekBurguer.Products.Contract.Models
+{
+    public class Item
+    {
+        public Guid ItemId { get; set; }
+        public string Name { get; set; }
+    }
+
+}
---
+++ GeekBurguer.Products.Contract/Models/ProductChanged.cs
@@ -0,0 +1,11 @@
+﻿using GeekBurguer.Products.Contract.Enums;
+
+namespace GeekBurguer.Products.Contract.Models
+{
+    public class ProductChanged
+    {
+        public ProductState State { get; set; }
+        public Product Product { get; set; }
+    }
+
+}
